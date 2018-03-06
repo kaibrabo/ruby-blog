5d9909453fbacccf8d14daab38169a9e3dabdc1f
Kai Brabo <kaibrabo@gmail.com>
2018-03-05 17:16:39 -0800

    added basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,54 +1,56 @@
 class ArticlesController < ApplicationController
     # index, show, new, edit, create, update, destroy
 
-def index
-    @articles = Article.all
-end
+    http_basic_authenticate_with name: 'kai', password: 'password', except: [:index, :show]
 
-def show
-    @article = Article.find(params[:id])
-end
-
-def new
-    @article = Article.new
-end
-
-def edit
-    @article = Article.find(params[:id])
-end
-
-def create
-    @article = Article.new(article_params)
-
-    if @article.save
-        redirect_to @article
-    else
-        render 'new'
+    def index
+        @articles = Article.all
     end
-end
 
-def update
-    @article = Article.new(params[:id])
-
-    if @article.update(article_params)
-        redirect_to @article
-    else
-        render 'edit'
+    def show
+        @article = Article.find(params[:id])
     end
-end
 
-def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
+    def new
+        @article = Article.new
+    end
 
-    redirect_to articles_path
-end
+    def edit
+        @article = Article.find(params[:id])
+    end
 
-private
+    def create
+        @article = Article.new(article_params)
 
-def article_params
-    params.require(:article)
-          .permit(:title, :text)
-end
+        if @article.save
+            redirect_to @article
+        else
+            render 'new'
+        end
+    end
+
+    def update
+        @article = Article.new(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+
+    private
+
+    def article_params
+        params.require(:article)
+              .permit(:title, :text)
+    end
 
 end
